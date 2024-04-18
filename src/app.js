e206db2a3a140a0e671c5bf001e6ddd9dd3f8b61
user <user@users-MacBook-Pro.local>
2024-04-18 09:19:19 +0530

    failure

--- src/app.js
+++ src/app.js
@@ -3,16 +3,19 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 
 const app = express();
-const port = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}));
+
 app.use(express.json({limit: "16kb"}));
 app.use(urlencoded({extended: true, limit: "16kb"}));
 app.use(express.static('public'));
 app.use(cookieParser());
 
-app.listen(port, () => {
-    console.log(`App Is Listening On ${port}`);
-})
+import userRoute from "./routes/user.routes.js";
+
+app.use("/api/v1/users", userRoute);
 
 export { app }
--- src/index.js
+++ src/index.js
@@ -2,13 +2,21 @@
 // import { DB_NAME } from "./constants.js";
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: './env'
 })
 
-connectDB();
-
+connectDB()
+.then(() => {
+  app.listen(process.env.PORT || 8000, () => {
+    console.log(`Server Is Running At Port ${process.env.PORT}`);
+  })
+})
+.catch((err)=>{
+  console.log("MongoDB Connection Failed", err);
+})
 
 
 
