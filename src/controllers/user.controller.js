a86c8e42b4b2c4d0e9b3f4416105e373a2215273
user <user@users-MacBook-Pro.local>
2024-04-20 15:52:24 +0530

    iglast

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,21 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import apiError from "../utils/apiError.js"
 
 const registerUser = asyncHandler( async (req, res) => {
-    res.status(200).json({
-        message: "Ok"
-    })
+    const { fullName, username, email, password } = req.body
+
+    if (
+        [fullName, username, email, password].some((fields)=>fields?.trim() === "")
+    ){
+        throw new apiError(401, "This Feild Cannot Be Empty");
+    }
+
+    const emailCheck = email.substring((email.length)-4, email.length)
+
+    if (emailCheck !== ".com" || email.at(email.length-10) !== "@"){
+        throw new apiError(400, "Your Email Is Invalid");
+    }
+
 } )
 
 export { registerUser }
